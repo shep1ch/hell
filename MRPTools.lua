637258ba4a74f8356846da26fefb6d77f84a3293
shep1ch <89019315+shep1ch@users.noreply.github.com>
2021-09-19 13:59:04 +0300

    Update MRPTools.lua

--- MRPTools.lua
+++ MRPTools.lua
@@ -24,7 +24,7 @@ local script_vers_text = "1.00"
 local update_url = "https://raw.githubusercontent.com/shep1ch/hell/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
 
-local script_url = "https://github.com/thechampguess/scripts/blob/master/autoupdate_lesson_16.luac?raw=true" -- тут свою ссылку
+local script_url = "https://raw.githubusercontent.com/shep1ch/hell/main/MRPTools.lua" -- тут свою ссылку
 local script_path = thisScript().path
 
 
